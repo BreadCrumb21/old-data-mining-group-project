c0906b058764ed1af083c6313755b29972283aa2
Jturner-code <54911366+Jturner-code@users.noreply.github.com>
2023-09-14 21:24:43 -0500

    Create main.py
    Little start code for the project

---
+++ main.py
@@ -0,0 +1,9 @@
+# Load libraries
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
+from sklearn.model_selection import train_test_split # Import train_test_split function
+from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+
+col_names = ['age','workclass','fnlwgt','education','education-num','marital-status','occupation','relationship','race','sex','capital-gain','capital-loss','hours-per-week','native-country','more-or-less']
+# load dataset
+test_data = pd.read_csv("adult.test", header=None, names=col_names)
