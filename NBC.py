da5f3c935807b4155b7ec61b1d03b74dcbf28241
Jturner-code <54911366+Jturner-code@users.noreply.github.com>
2023-10-05 16:05:29 -0500

    Update NBC.py
    got accuracy up to 74%

--- NBC.py
+++ NBC.py
@@ -3,18 +3,27 @@ import pandas as pd
 from sklearn.model_selection import train_test_split # Import train_test_split function
 from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import (
-    accuracy_score,
-    confusion_matrix,
-    ConfusionMatrixDisplay,
-    f1_score,
-    classification_report,
-)
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OrdinalEncoder
 
-data = pd.read_csv('hotData.csv')
-print(data.isna().sum())
-x = data.drop(columns=['Workclass_new', 'Education_new', 'Marital-status_new', 'Occupation_new', 'Relationship_new', 'Race_new', 'Sex_new','Native-country_new', 'Salary_new'])
-y = data['Salary_new']
+
+col_names = ['type','education','status','service','relation','race','gender','location','capital']
+# load dataset
+test_data = pd.read_csv("adultTestCorrected.test", header=None, names=col_names)
+# print(test_data.head())
+
+enc = OrdinalEncoder()
+#encoded_data = pd.DataFrame(enc.fit_transform(test_data))
+#print(encoded_data.head())
+enc.fit(test_data[["type","education","status","service","relation","race","gender","location","capital"]])
+test_data[["type","education","status","service","relation","race","gender","location","capital"]] = enc.fit_transform(
+    test_data[["type","education","status","service","relation","race","gender","location","capital"]])
+
+# print(test_data)
+
+feature_cols = ['type','status','relation','race','gender','education','service','location'] 
+x = test_data[feature_cols]
+y = test_data.capital
 
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size=0.33, random_state=125
